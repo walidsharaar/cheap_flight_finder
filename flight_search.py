d41e43da4b7b0a77fe91ecca92b476a16e1d29a8
Sharaar <walidsharar@gmail.com>
2021-07-05 18:33:34 +0200

    insert iata code of each city in sheet

--- flight_search.py
+++ flight_search.py
@@ -2,10 +2,17 @@
 import requests
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = "YOUR_API_KEY_HERE"
+TEQUILA_API_KEY = "sE396o55BU74Gtlp8szRoedd4_bEXCpL"
 
 class FlightSearch:
     def get_destination_code(self,city_name):
+        #print get distination code
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey":TEQUILA_API_KEY}
+        query={"term":city_name,"location_types":"city"}
+        response = requests.get(url=location_endpoint,headers=headers,params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
         # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
-        code="Testing"
+        # code="Testing"
         return code
